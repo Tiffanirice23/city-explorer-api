2e7aad4073e123555c433141cf6abc23807f15e5
Tiffani Rice <tiffanirice414@gmail.com>
2023-05-18 14:56:54 -0700

    test

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 'use strict'
-const axios = require ('axios');
+const axios = require('axios');
 
 console.log('My first server');
 
 const express = require('express');
 // const require = require('express/lib/request');
-require ('dotenv').config();
+require('dotenv').config();
 
 // let data = require('./data/weather.json');
 const cors = require('cors');
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3002;
 
 
 app.get('/', (request, response) => {
-  response.send ('Hello from my server!');
+  response.send('Hello from my server!');
 });
 
 app.get('/weather', async (request, response) => {
@@ -32,7 +32,7 @@ app.get('/weather', async (request, response) => {
 
   let url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}&days=5&units=I`
 
-  
+
   try {
     let weatherData = await axios.get(url);
     let longitude = weatherData.data.lon;
@@ -40,35 +40,60 @@ app.get('/weather', async (request, response) => {
     console.log(longitude);
     let weatherArray = weatherData.data.data.map(weatherProp => new Forecast(weatherProp, latitude, longitude));
     response.status(200).send(weatherArray);
-    
+
     /*let city = data.find(city => city.city_name.toLowerCase() === searchquery.toLowerCase());
     city.data.forEach(day => {
       let description = `Low of ${day.low_temp}, high of ${day.high_temp} with ${day.weather.description}`
       forecastArray.push(new Forecast(day.valid_date, description));
     });
     response.send(forecastArray);*/
-  } catch(e) {
+  } catch (e) {
     response.status(400).send("City not found");
   }
 })
 
-let movieURL = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.MOVIEDB_API_KEY}`;
-
-app.get('/movie', async (request, response) => {
- try {
-  let city = request.query.city;
-
-  let movieData = await axios.get(movieURL);
-  let responseData = movieData.data.results.filter(movie => movie.overview.includes(city));
-  console.log(city);
-  let mappedMovie = responseData.map(movieProp => new Movie (movieProp))
-  response.status(200).send(mappedMovie);
-
-} catch(e) {
-  response.status(400).send("Movie in your City not found");
+function parseWeathers(weatherData) {
+  try {
+    const weatherSummarize = weatherData.data.map(fiveDay => {
+      return new Forecast(fiveDay);
+    });
+    return Promise.resolve(weatherSummarize);
+  } catch (error) {
+    return Promise.reject(error);
+  }
 }
+
+function parseMovies(moviesData) {
+  // console.log("movdiesDATA HERE: ", moviesData);
+  try {
+    const movieSummarize = moviesData.map(oneMovie => {
+      return new Movie(oneMovie);
+    });
+    return Promise.resolve(movieSummarize);
+  } catch (e) {
+    return Promise.reject(e);
+  }
+}
+
+app.get('/movies', async (request, response, next) => {
+  try {
+    let city = request.query.cityName;
+    let movieURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIEDB_API_KEY}&query=${city}`;
+    console.log(movieURL);
+    let movieData = await axios.get(movieURL);
+    let movieMap = parseMovies(movieData.data.results);
+    movieMap.then(movie => {
+    response.status(200).send(movie);
+    })
+
+  } catch (e) {
+    response.status(400).send("Movie in your City not found");
+  }
 });
 
+app.get('*', (request, response) => {
+  response.send('On snap! The things you are looking for doesn\'t exist');
+})
 
 class Forecast {
   constructor(weatherProp, latitude, longitude) {
@@ -82,14 +107,13 @@ class Forecast {
 class Movie {
   constructor(movieProp) {
     this.title = movieProp.original_title;
-    this.release = movieProp.release_date;
     this.overview = movieProp.overview;
+    this.image_url = movieProp.poster_path;
+    this.release = movieProp.release_date;
+    this.popularity = movieProp.popularity;
   }
 }
 
-app.get('*', (request, response) =>{
-  response.send('On snap! The things you are looking for doesn\'t exist');
-})
 
 app.use((error, request, response, next) => {
   response.status(500).send({
